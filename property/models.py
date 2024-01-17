4a0e577aba42f074ac0fb23f1466fc69e028f47e
AlekseiEvgenievich <aleksei.smirnov@phystech.edu>
2024-01-17 16:12:07 +0100

    setup Flat changelist page.

--- property/models.py
+++ property/models.py
@@ -41,6 +41,8 @@ class Flat(models.Model):
 
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
+    new_building = models.BooleanField(null=True,
+                                       blank=True)
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
